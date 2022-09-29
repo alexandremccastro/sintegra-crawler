47f906383101e57e22ea0a2ead17c8814fd67826
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2022-09-29 13:03:41 -0300

    feat: base class for the spider crawler

---
+++ src/Core/Spider.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Core;
+
+use Http\Client;
+
+abstract class Spider
+{
+  /**
+   * Client used to make requests.
+   */
+  protected $httpClient;
+
+  /**
+   * The spider start point.
+   */
+  protected $siteURL = null;
+
+  /**
+   * The URL used to load captcha image.
+   */
+  protected $captchaURL = null;
+
+  /**
+   * The name of the input used to type captcha value.
+   */
+  protected $captchaInput = '';
+
+   /**
+   * The name of the input used to type cnpj value.
+   */
+  protected $cnpjInput = '';
+
+  /**
+   * Value of the input that triggers the search.
+   */
+  protected $searchTrigger = [];
+
+  /**
+   * The results retrieved from the page.
+   */
+  protected $results = [];
+
+
+  public function __construct(Client $httpClient, $siteURL, $captchaURL, $captchaInput, $cnpjInput, $searchTrigger)
+  {
+    $this->httpClient = $httpClient;
+    $this->siteURL = $siteURL;
+    $this->captchaURL = $captchaURL;
+    $this->captchaInput = $captchaInput;
+    $this->cnpjInput = $cnpjInput;
+    $this->searchTrigger = $searchTrigger;
+  }
+
+  public function prompt()
+  {
+    $this->plugIn();
+
+    $params = [];
+
+    if ($this->hasCaptcha()) {
+      $this->loadCaptcha();
+      echo "\033[01;31mO captcha foi salvo em: tmp/captcha.jpeg\033[0m\n";
+      $params[$this->captchaInput] = readline('Digite o texto do captcha: ');
+    }
+
+    $params[$this->cnpjInput] = readline('Digite o CNPJ da empresa: ');
+    $params = array_merge($params, $this->searchTrigger);
+    $content = $this->postInfo($params);
+    $this->results = $this->parseResults($content);
+  }
+
+  public function getResults()
+  {
+    return $this->results;
+  }
+
+  public abstract function plugIn();
+  public abstract function hasCaptcha();
+  public abstract function loadCaptcha();
+  public abstract function postInfo($params = []);
+  public abstract function parseResults($content);
+}
