2c76b7fb41dd8b914d74dc32f1f49db1ccd59b95
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2022-09-29 10:55:20 -0300

    feat: http client that will be used to make requests

---
+++ src/Http/Client.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Http;
+
+class Client
+{
+  /**
+   * Location where the cookie will be stored.
+   */
+  private $cookie = 'tmp/cookie';
+
+  public function get($url)
+  {
+    $ch = curl_init();
+
+    curl_setopt_array($ch, [
+      CURLOPT_URL => $url,
+      CURLOPT_HTTPGET => true
+    ]);
+
+    return $this->send($ch);
+  }
+
+  public function post($url, $params)
+  {
+    $ch = curl_init();
+
+    curl_setopt_array($ch, [
+      CURLOPT_URL => $url,
+      CURLOPT_POST => true,
+      CURLOPT_POSTFIELDS => http_build_query($params)
+    ]);
+
+    return $this->send($ch);
+  }
+
+  private function send($ch)
+  {
+    curl_setopt_array($ch, [
+      CURLOPT_SSL_VERIFYPEER => false,
+      CURLOPT_SSL_VERIFYHOST => false,
+      CURLOPT_COOKIEFILE => $this->cookie,
+      CURLOPT_COOKIEJAR => $this->cookie,
+      CURLOPT_TCP_KEEPALIVE => true,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_RETURNTRANSFER => true
+    ]);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+  }
+}
